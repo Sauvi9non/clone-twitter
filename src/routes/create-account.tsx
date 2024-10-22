699450be1d9ddd576b4b40d67c7701b9db915c63
sauvi9non <xxxdmstj@gmail.com>
2024-10-23 07:20:13 +0900

    유저 인증, 로그인 기능, 파이어베이스 연결

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,9 @@
 import { styled } from "styled-components";
 import React, { useState } from "react";
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 
 const Wrapper = styled.div`
     heigth: 100vh;
@@ -45,6 +49,7 @@ const Error = styled.span`
 
 
 export default function CreateAccount() { //계정 생성 페이지
+    const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -65,14 +70,22 @@ export default function CreateAccount() { //계정 생성 페이지
 
     const onSubmit = async(e:React.FormEvent<HTMLFormElement>) => { //form태그가 제출될 때 실행
         e.preventDefault(); //화면이 새로고침되는 것을 막는다.
+        if( isLoading && name === "" || email === "" || password === ""){ //name, email, password 중 하나라도 비어있으면
+            return; //함수 종료
+        }
+        setIsLoading(true);
         try{
             //create an account
+            const credentials = await createUserWithEmailAndPassword(auth, email, password); //파이어베이스에 이메일과 비밀번호로 계정을 생성한다.
+            console.log(credentials.user)
             //set the user of the name on firebase
+            await updateProfile(credentials.user, {displayName: name}); //파이어베이스에 계정의 이름을 설정한다.
             //redirect to home
+            navigate("/"); 
         } catch(e){
             //setError(e.message);
         } finally {
-            setIsLoading(true); //로딩중으로 바꾼다.
+            setIsLoading(false);
         }
         
 
