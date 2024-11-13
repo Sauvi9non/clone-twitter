96b3c21c882f50f68473a3ffaf0b88a976bf00b0
sauvi9non <xxxdmstj@gmail.com>
2024-11-13 21:30:03 +0900

    tweet 삭제 기능

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,5 +1,7 @@
 import { styled } from "styled-components";
 import { ITweet } from "./timeline";
+import { deleteDoc, doc } from "firebase/firestore";
+import { auth, database } from "../firebase";
 
 const Wrapper = styled.div`
     display: grid;
@@ -24,16 +26,44 @@ const Payload = styled.p`
 const Column = styled.div`
 `;
 
-export default function Tweet({username, tweet}:ITweet){
+const DeleteButton = styled.button`
+    padding: 5px;
+    margin: 5px;
+    background-color: #ff0000;
+    cursor: pointer;
+    font-size: 16px;
+    &:focus {
+        outline: none;
+    }
+`;
+
+export default function Tweet({username, tweet, userId, id}:ITweet){
+    const user = auth.currentUser;
+    
+    const onDelete = async () => {
+        const check: boolean = confirm(`${id} ${userId} 트윗을 삭제하시겠습니까?`);
+        if ( !check || user === null ||user.uid !== userId) return; //user가 null이거나 uid가 다르면 함수 종료
+        try {
+                await deleteDoc(doc(database,`tweets/${id}`));
+        } catch(e) {
+            console.log(e);
+        } finally {
+//
+        }
+    }
     return(
         <Wrapper>
-            <Column><Nickname>{username}</Nickname></Column>
-            <Column><Payload>{tweet}</Payload></Column>
+            <Column>
+            <Nickname>{username}</Nickname>
+            <Payload>{tweet}</Payload>
+            { user?.uid == userId ? <DeleteButton onClick={onDelete}>Delete</DeleteButton> : null}
+            </Column>
             {/**
              * if (photo)
              *  <Column><Photo src={file}></Photo></Column>
              * 나중에 파일 업로드 되는거 고치면 하기
              */}
+             
         </Wrapper>
     );
 }
