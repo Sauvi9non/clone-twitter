c11580ab770b76594769c3431de231e5a211b67b
sauvi9non <xxxdmstj@gmail.com>
2024-11-14 23:22:50 +0900

    프로필 페이지에서 사용자 닉네임 변경
    *기존 트윗의 데이터에 있는 닉네임 변경은 못함

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -5,7 +5,8 @@ import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { updateProfile } from "firebase/auth";
 import { ITweet } from "../components/timeline";
 import Tweet from "../components/tweet";
-import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { collection, doc, getDocs, limit, orderBy, query, updateDoc, where } from "firebase/firestore";
 
 const Wrapper = styled.div`
   display: flex;
@@ -39,6 +40,12 @@ const UserName = styled.span`
   font-size: 24px;
   font-weight: 600;
 `;
+
+const Name = styled.div`
+  display: flex;
+  flex-direction: row;;
+`;
+
 const UserEmail = styled.span`
   font-size: 18px;
 `;
@@ -52,6 +59,23 @@ const MyTweet = styled.div`
   flex-direction: column;
   gap: 30px;
 `
+const EditButton = styled.button`
+  width: 24px;
+  padding: 1px;
+  opacity: 100;
+  border-radius: 50px;
+  cursor: pointer;
+
+.svg {
+  fill: white;
+  maring: 0;
+  width: 100%;
+}
+  &focus {
+    outline: none;
+
+  }
+`;
 
 export default function Profile() {
   const user = auth.currentUser;
@@ -92,13 +116,32 @@ export default function Profile() {
     }
   }
 
+  const onClick = async () => {
+    if (!user) return;
+    const newDisplayName = prompt("변경할 닉네임을 입력하세요");
+    //const nameRef = doc(database, `tweets` );
+    //await updateDoc(nameRef, {username: newDisplayName}); //기존 트윗들 데이터 변경
+    await updateProfile(user, { //사용자 프로필 변경
+      displayName: newDisplayName
+    })
+  }
+
   return (
     <Wrapper>
       <AvatarUpload htmlFor="avatar">
         { <AvatarImg src={avatar ? avatar : defaultImg} />}
       </AvatarUpload> {/** Input에 id를 주어야 label과 연결이 가능 */}
       <AvatarInput id="avatar" type="file" accept="image/*" onChange={onAvatarChange}></AvatarInput>
+      
+      <Name>
       <UserName>{user?.displayName}</UserName>
+      <EditButton onClick={onClick}>
+      <svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"></path>
+</svg>
+      </EditButton>
+      </Name>
+
       <UserEmail>{user?.email}</UserEmail>
 
       <MyTweet>
